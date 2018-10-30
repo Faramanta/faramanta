c69b6ae8941e7f98e03c24323f2247d50815dedb
Faramanta <katuxa@yandex.ru>
2018-10-30 22:29:56 +0500

    cats

--- cats/js/main.js
+++ cats/js/main.js
@@ -3,15 +3,16 @@ var cardBlock = document.querySelectorAll('.card__block');
 
 
 for (var i = 0; i < card.length; i++) {
-    card[i].addEventListener('click', function () {
-        if (!(this.parentNode.getAttribute('data-available') === 'disabled')) {
-            this.parentNode.classList.toggle('selected');
-            if (this.parentNode.classList.contains('selected')) {
+    card[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        if (!(this.closest('.card__block').getAttribute('data-available') === 'disabled')) {
+            this.closest('.card__block').classList.toggle('selected');
+            if (this.closest('.card__block').classList.contains('selected')) {
                 this.addEventListener('mouseleave', function () {
-                    if (this.parentNode.classList.contains('selected')) {
-                        this.parentNode.classList.add('hover');
+                    if (this.closest('.card__block').classList.contains('selected')) {
+                        this.closest('.card__block').classList.add('hover');
                     } else {
-                        this.parentNode.classList.remove('hover');
+                        this.closest('.card__block').classList.remove('hover');
                     }
                 });
             }
